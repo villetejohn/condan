1960d3b186de6b689289405b391a27026652c510
John Villete <jvillete@brewedlogic.com>
2018-07-03 19:08:28 +0800

    Added color property in booked amenities api

--- routes/api.js
+++ routes/api.js
@@ -6,12 +6,28 @@ var moment = require('moment');
 //Router: api/booked-amenities
 // GET API Request
 router.get('/booked-amenities', function(req, res, next) {
-  Amenity.find({}, null, {sort: 'scheduleStart'}, function(err, bookingDetails) {
+  Amenity.find({status: 'Approved'}, null, {sort: 'scheduleStart'}, function(err, bookingDetails) {
     var newBookingDetails = bookingDetails.map(bookDetail => {
+      var color;
+      switch (bookDetail.amenity) {
+        case 'Pool':
+            color = '#007bff';
+            break;
+        case 'Karaoke Room':
+            color = '#ffc107';
+            break;
+        case 'Gym':
+            color = '#28a745';
+            break;
+        default:
+             color = '#6c757d';
+            break;
+      }
       return {
         title: `${bookDetail.bookedByName} | ${bookDetail.amenity}`,
         start: bookDetail.scheduleStart,
-        end: moment(bookDetail.scheduleEnd)
+        end: moment(bookDetail.scheduleEnd),
+        color: color
       }  
     });
 
