7ea5cc88e5ca66954af377c18c94da4dd89eebf4
John Villete <jvillete@brewedlogic.com>
2018-07-04 21:56:59 +0800

    Added type, unit fields in user model

--- models/user.js
+++ models/user.js
@@ -5,6 +5,14 @@ var UserSchema = mongoose.Schema({
         type: String,
         required: true
     },
+    type: {
+        type: String,
+        required: true
+    },
+    unit: {
+        type: String,
+        required: true
+    },
     email: {
         type: String,
         required: true
@@ -21,6 +29,11 @@ var UserSchema = mongoose.Schema({
         type: Boolean,
         default: false,
         required: true,
+    },
+    is_validated: {
+        type: Boolean,
+        default: false,
+        required: true
     }
 });
 
