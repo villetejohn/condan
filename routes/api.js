cca8b011abcb153f0eee10d5d1f9766785575b89
John Villete <jvillete@brewedlogic.com>
2018-07-04 13:00:25 +0800

    Fixed Api access control

--- routes/api.js
+++ routes/api.js
@@ -6,6 +6,11 @@ var moment = require('moment');
 //Router: api/booked-amenities
 // GET API Request
 router.get('/booked-amenities', function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  
   Amenity.find({status: 'Approved'}, null, {sort: 'scheduleStart'}, function(err, bookingDetails) {
     var newBookingDetails = bookingDetails.map(bookDetail => {
       var color;
