db938e3088c108466b80f2a0884420bde4e64cd7
John Villete <jvillete@brewedlogic.com>
2018-07-06 09:47:22 +0800

    Added table filter in view-reports, accounts, announcements

--- public/javascripts/tablefilter.js
+++ public/javascripts/tablefilter.js
@@ -1,10 +1,34 @@
 $(function() {
     $(document).ready(function(){
-      $("#incidents-table-filter").on("keyup", function() {
-        var value = $(this).val().toLowerCase();
-        $("#incidents-table-body tr").filter(function() {
-          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-        });
-      });
+
+      switch (window.location.pathname) {
+        case '/dashboard/view-reports':
+          $("#table-filter").on("keyup", function() {
+            var value = $(this).val().toLowerCase();
+            $("#incidents-table-body tr").filter(function() {
+              $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            });
+          });
+          break;
+
+        case '/dashboard/accounts' :
+          $("#table-filter").on("keyup", function() {
+            var value = $(this).val().toLowerCase();
+            $("#users-table-body tr").filter(function() {
+              $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            });
+          });
+          break;
+
+        case '/dashboard/home' :
+          $("#table-filter").on("keyup", function() {
+            var value = $(this).val().toLowerCase();
+            $("#announcement-body section").filter(function() {
+              $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            });
+          });
+          break;
+      }
+
     });
 });
